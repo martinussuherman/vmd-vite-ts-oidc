262969b26278d76b9725aa01c9fda647f547449e
Martinus Suherman <martinus.suherman@gmail.com>
2021-03-12 12:08:55 +0700

    Add vmd base components

---
+++ src/components/base/ItemSubGroup.vue
@@ -0,0 +1,22 @@
+<template>
+  <base-item-group :item="item" text sub-group />
+</template>
+
+<script>
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+export default {
+  name: "ItemSubGroup",
+
+  props: {
+    item: {
+      type: Object,
+      default: () => ({
+        avatar: undefined,
+        group: undefined,
+        title: undefined,
+        children: [],
+      }),
+    },
+  },
+};
+</script>
