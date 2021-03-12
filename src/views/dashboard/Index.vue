350ac77f966ad19bef73bef3bf93a8def091bb64
Martinus Suherman <martinus.suherman@gmail.com>
2021-03-12 12:10:11 +0700

    Add vmd dasboard views

---
+++ src/views/dashboard/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <dashboard-core-app-bar />
+
+    <dashboard-core-drawer />
+
+    <dashboard-core-view />
+
+    <dashboard-core-settings />
+  </v-app>
+</template>
+
+<script>
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+export default {
+  name: "DashboardIndex",
+
+  components: {
+    DashboardCoreAppBar: () => import("./components/core/AppBar"),
+    DashboardCoreDrawer: () => import("./components/core/Drawer"),
+    DashboardCoreSettings: () => import("./components/core/Settings"),
+    DashboardCoreView: () => import("./components/core/View"),
+  },
+
+  data: () => ({
+    expandOnHover: false,
+  }),
+};
+</script>
---
+++ src/views/dashboard/components/core/View.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-main>
+    <router-view />
+
+    <dashboard-core-footer />
+  </v-main>
+</template>
+
+<script>
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+export default {
+  name: "DashboardCoreView",
+
+  components: {
+    DashboardCoreFooter: () => import("./Footer"),
+  },
+};
+</script>
