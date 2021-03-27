e885d92159bc44b796ddde312872c5565b75d458
Martinus Suherman <martinus.suherman@gmail.com>
2021-03-27 19:37:10 +0700

    Use import.meta.env.BASE_URL for images location

--- src/views/dashboard/components/core/Settings.vue
+++ src/views/dashboard/components/core/Settings.vue
@@ -163,17 +163,17 @@ export default {
   data: () => ({
     color: "#E91E63",
     colors: ["#9C27b0", "#00CAE3", "#4CAF50", "#ff9800", "#E91E63", "#FF5252"],
-    image: "/pexels-brady-knoll-5187131.jpg",
+    image: `${import.meta.env.BASE_URL}/pexels-brady-knoll-5187131.jpg`,
 
     images: [
       // Photo by Brady Knoll from Pexels - https://www.pexels.com/photo/narrow-pathway-through-coniferous-trees-in-forest-5187131/
-      "/pexels-brady-knoll-5187131.jpg",
+      `${import.meta.env.BASE_URL}/pexels-brady-knoll-5187131.jpg`,
       // Photo by Alex Andrews from Pexels - https://www.pexels.com/photo/photo-of-deep-sky-object-816608/
-      "/pexels-alex-andrews-816608.jpg",
+      `${import.meta.env.BASE_URL}/pexels-alex-andrews-816608.jpg`,
       // Photo by Zhaocan Li from Pexels - https://www.pexels.com/photo/snow-covered-mountain-1755243/
-      "/pexels-zhaocan-li-1755243.jpg",
+      `${import.meta.env.BASE_URL}/pexels-zhaocan-li-1755243.jpg`,
       // Photo by Antonio Dillard from Pexels - https://www.pexels.com/photo/exciting-art-composition-on-yellow-background-4462780/
-      "/pexels-antonio-dillard-4462780.jpg",
+      `${import.meta.env.BASE_URL}/pexels-antonio-dillard-4462780.jpg`,
     ],
     menu: false,
     showImg: true,
@@ -192,7 +192,7 @@ export default {
       if (val) {
         this.setBarImage(this.image);
       } else {
-        this.setBarImage("/blank.png");
+        this.setBarImage(`${import.meta.env.BASE_URL}/blank.png`);
       }
     },
     image(val) {
