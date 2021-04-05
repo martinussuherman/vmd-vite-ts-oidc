8309ae737189402c27c0335ffdd9a613950d6db8
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-05 15:32:37 +0700

    Update vuetify configuration
    - Create theme configuration
    - Use ripple

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,30 @@
 import Vue from "vue";
-import Vuetify from "vuetify/lib/framework";
-import "@/sass/overrides.sass";
+import Vuetify from "vuetify/lib";
+import { Ripple } from "vuetify/lib/directives";
+import { VuetifyThemeVariant } from "vuetify/types/services/theme";
+// import i18n from "@/i18n";
+// import "@/sass/overrides.sass";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+const theme: Partial<VuetifyThemeVariant> = {
+  primary: "#4CAF50",
+  secondary: "#9C27b0",
+  accent: "#9C27b0",
+  info: "#00CAE3",
+};
+
+export default new Vuetify({
+  // lang: {
+  //   t: (key, ...params) => i18n.t(key, params) as string,
+  // },
+  theme: {
+    themes: {
+      dark: theme,
+      light: theme,
+    },
+  },
+  directives: {
+    Ripple,
+  },
+});
